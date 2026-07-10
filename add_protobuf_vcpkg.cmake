d7cbe923aa601a5c374d29aeef1870ee748356a6
martynov_ag <martynov_ag@nectech.pro>
2026-07-10 18:31:07 +0300

    Попытка пофиксить варнинги протобуфа (не вышло, но пусть будет). Добавил номеров тулсетов MSVC

--- add_protobuf_vcpkg.cmake
+++ add_protobuf_vcpkg.cmake
@@ -6,6 +6,21 @@
 include_guard(GLOBAL)
 
 find_package(Protobuf CONFIG REQUIRED)
+set_target_properties(protobuf::libprotobuf PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "$<TARGET_PROPERTY:protobuf::libprotobuf,INTERFACE_INCLUDE_DIRECTORIES>")
+#set_target_properties(protobuf::libprotobuf-lite PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "$<TARGET_PROPERTY:protobuf::libprotobuf-lite,INTERFACE_INCLUDE_DIRECTORIES>")
+# Используйте код с осторожностью.Этот метод гарантирует, что компилятор проигнорирует C4371 исключительно внутри кода Protobuf.Способ 2. Локальное подавление в коде (Если предупреждение идет из .pb.h файлов)Если предупреждение вызывают файлы, сгенерированные утилитой protoc (.pb.h), вы можете обернуть их включение в прагмы компилятора. Это отключит предупреждение только на момент чтения этих файлов:cpp#if defined(_MSC_VER)
+#     #pragma warning(push)
+#     #pragma warning(disable : 4371) // Отключаем C4371 только для Protobuf
+# #endif
+# 
+# #include "your_file.pb.h" // Подключение сгенерированного файла
+# 
+# #if defined(_MSC_VER)
+#     #pragma warning(pop) // Возвращаем исходные настройки предупреждений для вашего кода
+# #endif
+# Используйте код с осторожностью.Способ 3. Синхронизация стандартов C++Проверьте, какой стандарт C++ задан в вашем проекте. Попробуйте явно выставить в CMakeLists.txt тот же стандарт, с которым vcpkg собирает современные библиотеки (рекомендуется C++17 или C++20):cmakeset(CMAKE_CXX_STANDARD 17)
+# set(CMAKE_CXX_STANDARD_REQUIRED ON)
+# Используйте код с осторожностью.Чтобы помочь точнее, подскажите: предупреждение C4371 указывает на какой-то конкретный файл (например, заголовок из недр vcpkg или ваш сгенерированный .pb.h)? Какой стандарт C++ сейчас настроен в вашем CMakeLists.txt?
 
 get_target_property(PROTOBUF_INC_PATH protobuf::libprotobuf INTERFACE_INCLUDE_DIRECTORIES)
 if(NOT PROTOBUF_INC_PATH)
